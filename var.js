87bb24c1c9899fa049ca74330cbee623775ff2b7
moonsung1234 <dit67890@gmail.com>
2023-09-13 19:27:37 +0900

    init

--- var.js
+++ var.js
@@ -2,7 +2,7 @@
 module.exports = {
     DEFAULT_WIDTH : 2000,
     DEFAULT_HEIGHT : 1000,
-    DELAY : 1000 * 60, // 1 minute
+    DELAY : 1000 * 60 * 5, // 5 minute
     ENV_PATH : "env.json",
     POST_PATH : "post.png",
     EXECUTABLE_PATH : "/usr/bin/chromium-browser",
