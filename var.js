3bb06fe9f34c84e9b3d2e2efdca96b157d10bda2
moonsung1234 <dit67890@gmail.com>
2023-09-14 15:00:59 +0900

    update saving env in Redis

--- var.js
+++ var.js
@@ -2,8 +2,8 @@
 module.exports = {
     DEFAULT_WIDTH : 2000,
     DEFAULT_HEIGHT : 1000,
-    DELAY : 1000 * 60 * 2, // 1 minute
-    EXECUTABLE_PATH : "", //"/usr/bin/chromium-browser",
+    DELAY : 1000 * 60 * 2, // 2 minute
+    EXECUTABLE_PATH : "/usr/bin/chromium-browser",
     VIDEO_TYPE : Symbol("video"),
     TEXT_TYPE : Symbol("text")
 }
